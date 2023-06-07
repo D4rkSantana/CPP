fb640f8ea107c3d00b093642d80c96e2634207fd
S4ntz <s4ntz@localhost.localdomain>
2023-06-07 12:52:06 -0300

    pdfs add

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,21 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: esilva-s < esilva-s@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 00:40:38 by esilva-s          #+#    #+#             */
-/*   Updated: 2023/04/23 21:33:14 by esilva-s         ###   ########.fr       */
+/*   Updated: 2023/06/07 11:38:03 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include <iostream>
+#include <string>
 #include "replace.hpp"
+
+//recebe 3 parametros (filename; string1; string2)
+//subtistitui todas as ocorrencias de s1 por s2
+//sava o resultado em um novo arquivo chamado filename.replace
 
 static int	check_args(int argc)
 {
 	if (argc != 4)
 	{
 		std::cout << "Only 3 arguments bro!" << std::endl;
-		std::cout << "./a.out <filename> <string1> <string2>" << std::endl;
+		std::cout << "./my_replace <filename> <string1> <string2>" << std::endl;
 		return (1);
 	}
 	return (0);
@@ -26,6 +33,7 @@ static int	check_args(int argc)
 int	main(int argc, char **argv)
 {
 	std::string	content;
+	std::string	result;
 
 	if (check_args(argc))
 		return (1);
@@ -33,20 +41,20 @@ int	main(int argc, char **argv)
 	content = read_file(argv[1]);
 	if (content == "")
 	{
-		std::cout << "File Error" << std::endl;
+		std::cout << "File empyt" << std::endl;
 		return (1);
 	}
-
 	std::cout << content;
+	result = replace(content, argv[2], argv[3])
 	return (0);
 }
 
 //checar quantidade de argumentos -- OK
 
-//abrir arquivo
-	//se não conseguir retornar erro
+//abrir arquivo -- OK
+	//se não conseguir retornar erro -- OK
 
-//salvar dados do arquivo
+//salvar dados do arquivo -- OK
 	//se estiver vazio retornar erro
 
 //fazer substituições
--- cpp01/ex04/replace.cpp
+++ cpp01/ex04/replace.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   replace.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: esilva-s < esilva-s@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 20:18:45 by esilva-s          #+#    #+#             */
-/*   Updated: 2023/04/23 21:30:41 by esilva-s         ###   ########.fr       */
+/*   Updated: 2023/06/07 12:51:25 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,3 +31,52 @@ std::string	read_file(std::string file_name)
 	file.close();
 	return (result);
 }
+
+std::string replace(std::string content, std::string s1, std::string s2)
+{
+	std::string	result;
+	size_t	pos = 0;
+	size_t	pos_temp = 0;
+
+	pos_temp = content.find(s1, pos);
+	if ()
+	while (){}
+}
+
+/*
+// string::substr
+#include <iostream>
+#include <string>
+
+int main ()
+{
+    std::string result;
+    std::string str="We think in generalities, but we live in details.";
+    std::string s1 = " ";
+    std::string s2 = ".x.";
+                                           // (quoting Alfred N. Whitehead)
+    size_t  pos = 0;
+    size_t  temp = 0;
+    
+    while (pos < str.size())
+    {
+        temp = str.find(s1, pos);
+        if (temp == std::string::npos)
+        {
+            result.append(str.substr(pos, str.size()));
+            break ;
+        }
+        else
+        {
+            result.append(str.substr(pos, temp));
+            std::cout << result << "|" << "pos: " << pos << " temp: " << temp << std::endl;
+            result.append(s2);
+            std::cout << result << "|" << "pos: " << pos << " temp: " << temp << std::endl;
+            pos = temp + s1.size();
+            std::cout << "--- pos: " << pos << " temp: " << temp << std::endl;
+        }
+    }
+    std::cout << "result: " << std::endl << result << std::endl;
+  return 0;
+}
+*/
