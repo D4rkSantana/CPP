88c736d20977186c79947933119fa246e4fb5261
D4rkSantana <emersonss13@gmail.com>
2023-07-10 14:36:53 -0300

    fix templates

--- ex0/Template.cpp
+++ ex0/Template.cpp
@@ -6,18 +6,28 @@
 /*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 19:36:00 by esilva-s          #+#    #+#             */
-/*   Updated: 2023/06/13 19:36:31 by esilva-s         ###   ########.fr       */
+/*   Updated: 2023/07/10 14:29:05 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-Template()
-{}
+#include "Template.hpp"
 
-Template(const Template& temp)
-{}
+Template::Template()
+{
+    
+}
 
-Template& operator=(const Template& temp)
-{}
+Template::Template(const Template& temp)
+{
 
-~Template()
-{}
+}
+
+Template &Template::operator=(const Template& temp)
+{
+    
+}
+
+Template::~Template()
+{
+    
+}
