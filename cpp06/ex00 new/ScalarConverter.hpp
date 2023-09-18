778fb4828fd26988dec2984cb943b6598a80454e
s4ntz <emersonss13@gmail.com>
2023-09-18 16:36:35 -0300

    refazendo o cpp06

---
+++ cpp06/ex00 new/ScalarConverter.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 19:21:25 by esilva-s          #+#    #+#             */
+/*   Updated: 2023/08/29 00:25:58 by esilva-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ScalarConverter_HPP
+#define ScalarConverter_HPP
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+
+class ScalarConverter
+{
+    private:
+
+    public:
+        ScalarConverter();
+        ScalarConverter(const ScalarConverter& temp);
+        ScalarConverter& operator=(const ScalarConverter& temp);
+        ~ScalarConverter();
+
+		static void convert(const std::string &literal);
+};
+
+std::ostream&   operator<<( std::ostream &out, const ScalarConverter &ref);
+
+#endif /*ScalarConverter_HPP*/
