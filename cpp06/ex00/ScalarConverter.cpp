9d8682933095fabba20e6fa35020c9cba1c23dee
s4ntz <emersonss13@gmail.com>
2023-09-22 22:27:50 -0300

    finish cpp06 ex00

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -65,7 +65,7 @@ bool checkChar(const std::string &literal)
     return (false);
 }
 
-bool checkNumeric(const std::string &literal)
+bool checkInt(const std::string &literal)
 {
     size_t index = 0;
 
@@ -74,32 +74,102 @@ bool checkNumeric(const std::string &literal)
         
     while (index < literal.size())
     {
-        if (std::isdigit(literal[index]) == 0 && literal[index] != '.')
+        if (std::isdigit(literal[index]) == 0)
             return (false);
         index++;
     }
     return (true);
 }
 
-char    checkType(const std::string &literal)
+bool checkFloat(const std::string &literal)
+{
+    size_t  index = 0;
+    bool    sulf_f = false;
+    bool    point = false;
+
+    if (literal[0] == '-' || literal[0] == '+')
+        index++;
+    while (index < literal.size())
+    {
+        if (std::isdigit(literal[index]) == 0 && literal[index] == '.')
+        {
+            if (!point && index + 1 < literal.size())
+            {
+                if (std::isdigit(literal[index + 1] == 0))
+                    return (false);
+                point = true;
+            }
+            else
+                return (false);
+        }
+        else if (std::isdigit(literal[index]) == 0 && literal[index] == 'f')
+        {
+            if (!sulf_f)
+                sulf_f = true;
+            else
+                return (false);
+        }
+        else if (std::isdigit(literal[index]) == 0)
+            return (false);
+        index++;
+    }
+    if (point && sulf_f)
+        return (true);
+    return (false);
+}
+
+bool checkDouble(const std::string &literal)
+{
+    size_t  index = 0;
+    bool    comma = false;
+
+    if (literal[0] == '-' || literal[0] == '+')
+        index++;
+    while (index < literal.size())
+    {
+        if (std::isdigit(literal[index]) == 0 && literal[index] == ',')
+        {
+            if (!comma && index + 1 < literal.size())
+            {
+                if (std::isdigit(literal[index + 1] == 0))
+                    return (false);
+                comma = true;
+            }
+            else
+                return (false);
+        }
+        else if (std::isdigit(literal[index]) == 0)
+            return (false);
+        index++;
+    }
+    if (comma)
+        return (true);
+    return (false);
+}
+
+char checkType(const std::string &literal)
 {
     if (checkPseudo(literal))
         return ('p');
     else if (checkChar(literal))
         return ('c');
-    else if (checkNumeric(literal))
-        return ('n');
+    else if (checkInt(literal))
+        return ('i');
+    else if (checkFloat(literal))
+        return ('f');
+    else if (checkDouble(literal))
+        return ('d');
     return ('e');
 }
 
 //========================= print functions =========================//
 
-void    printOriginChar(const std::string &literal)
+void    convertOriginChar(const std::string &literal)
 {
     char outChar = static_cast<char>(literal[0]);
-    int outInt = static_cast<int>(literal[0]);
-    float outFloat = static_cast<float>(outInt);
-    double outDouble = static_cast<double>(outInt);
+    int outInt = static_cast<int>(outChar);
+    float outFloat = static_cast<float>(outChar);
+    double outDouble = static_cast<double>(outChar);
 
     std::cout << "char:   \'" << outChar << "\'" << std::endl;
     std::cout << "int:    " << outInt << std::endl;
@@ -107,7 +177,152 @@ void    printOriginChar(const std::string &literal)
     std::cout << std::fixed << std::setprecision(1) << "double: " << outDouble << std::endl;
 }
 
-bool    printOriginNumeric(const std::string &literal)
+bool    convertOriginInt(const std::string &literal)
+{
+    int         outInt;
+    char        outChar;
+    float       outFloat;
+    double      outDouble;
+    long long   convert_aux;
+
+    //testa o limite do tipo
+    convert_aux = std::atoll(literal.c_str());
+    if (convert_aux > INT_MAX || convert_aux < INT_MIN)
+    {
+        std::cout << "the integer type number is out of range\n";
+        return (false);
+    }
+    //converte de string para o tipo de origem
+    outInt = static_cast<int>(std::atoi(literal.c_str()));
+    //converte de int para char
+    if (outInt >= 32 && outInt <= 126)
+    {
+        outChar = static_cast<char>(outInt);
+        std::cout << "char:   \'" << outChar << "\'" << std::endl;
+    }
+    else
+        std::cout << "char:   Non displayable" << std::endl;
+    //exibe o valor do int
+    std::cout << "int:    " << outInt << std::endl;
+    //converte de int para float
+    if (outInt >= -FLT_MAX && outInt <= FLT_MAX)
+    {
+        outFloat = static_cast<float>(outInt);
+        std::cout << std::fixed << std::setprecision(1) << "float:  " << outFloat << "f\n";
+    }
+    else
+        std::cout << "float:  " << "the range has been exceeded\n";
+    //converte de int para double
+    if (outInt <= DBL_MAX && outInt >= -DBL_MAX)
+    {
+        outDouble = static_cast<double>(outInt);
+        std::cout << std::fixed << std::setprecision(1) << "double: " << outDouble << std::endl;
+    }
+    else
+        std::cout << "double: " << "the range has been exceeded\n";
+    return (true);
+}
+
+bool    convertOriginFloat(const std::string &literal)
+{
+    int         outInt;
+    char        outChar;
+    float       outFloat;
+    double      outDouble;
+    long double convert_aux;
+
+    //testa o limite do tipo
+    convert_aux = std::strtold(literal.c_str(), NULL);
+    if (convert_aux < -FLT_MAX || convert_aux > FLT_MAX)
+    {
+        std::cout << "the float type number is out of range\n";
+        return (false);
+    }
+
+    //converte de string para o tipo de origem
+    outFloat = static_cast<float>(std::atof(literal.c_str()));
+
+    //converte de int para char
+    if (outFloat >= 32.0 && outFloat <= 126.0)
+    {
+        outChar = static_cast<char>(outFloat);
+        std::cout << "char:   \'" << outChar << "\'" << std::endl;
+    }
+    else
+        std::cout << "char:   Non displayable" << std::endl;
+
+    //converte de float para int
+    if (outFloat <= INT_MAX && outFloat >= INT_MIN)
+    {
+        outInt = static_cast<int>(outFloat);
+        std::cout << "int:    " << outInt << std::endl;
+    }
+    else
+        std::cout << "int:    " << "the range has been exceeded\n";
+
+    //exibe o valor do float
+    std::cout << std::fixed << std::setprecision(1) << "float:  " << outFloat << "f\n";
+
+    //converte de float para double
+    outDouble = static_cast<double>(outFloat);
+    std::cout << std::fixed << std::setprecision(1) << "double: " << outDouble << std::endl;
+
+    return (true);
+}
+
+bool    convertOriginDouble(const std::string &literal)
+{
+    int         outInt;
+    char        outChar;
+    float       outFloat;
+    double      outDouble;
+    long double convert_aux;
+
+    //testa o limite do tipo
+    convert_aux = std::strtold(literal.c_str(), NULL);
+    if (convert_aux < -DBL_MAX || convert_aux > DBL_MAX)
+    {
+        std::cout << "the double type number is out of range\n";
+        return (false);
+    }
+
+    //converte de string para o tipo de origem
+    outDouble = static_cast<double>(std::strtod(literal.c_str(), NULL));
+
+    //converte de double para char
+    if (outDouble >= 32.0 && outDouble <= 126.0)
+    {
+        outChar = static_cast<char>(outDouble);
+        std::cout << "char:   \'" << outChar << "\'" << std::endl;
+    }
+    else
+        std::cout << "char:   Non displayable" << std::endl;
+
+    //converte de double para int
+    if (outDouble <= INT_MAX && outDouble >= INT_MIN)
+    {
+        outInt = static_cast<int>(outDouble);
+        std::cout << "int:    " << outInt << std::endl;
+    }
+    else
+        std::cout << "int:    " << "the range has been exceeded\n";
+
+    //convert de double para float
+    if (outDouble >= -FLT_MAX && outDouble <= FLT_MAX)
+    {
+        outFloat = static_cast<float>(outDouble);
+        std::cout << std::fixed << std::setprecision(1) << "float:  " << outFloat << "f\n";
+    }
+    else
+        std::cout << "float:  " << "the range has been exceeded\n";
+
+    //exibe o double
+    std::cout << std::fixed << std::setprecision(1) << "double: " << outDouble << std::endl;
+
+    return (true);
+}
+
+bool    convertOriginNumeric(const std::string &literal)
 {
     char outChar;
     int outInt = 0;
@@ -139,7 +354,9 @@ bool    printOriginNumeric(const std::string &literal)
 
     if (ld_aux >= -FLT_MAX && ld_aux <= FLT_MAX)
     {
-        outFloat = static_cast<float>(ld_aux);
+        long long test = static_cast<long long>(ld_aux);
+        std::cout << "long long: " << test << std::endl;
+        outFloat = static_cast<float>(test);
         std::cout << std::fixed << std::setprecision(1) << "float:  " << outFloat << "f\n";
     }
     else
@@ -170,17 +387,30 @@ void ScalarConverter::convert(const std::string &literal)
         std::cout << "double: "<< literal << std::endl;
     }
     else if (result == 'c')
-        printOriginChar(literal);
-    else if (result == 'n')
+        convertOriginChar(literal);
+    else if (result == 'i')
     {
-        if (!printOriginNumeric(literal))
+        if (!convertOriginInt(literal))
+            std::cout << "An error occurred, please try again using an int, char, float or double literal\n";
+    }
+    else if (result == 'f')
+    {
+        if (!convertOriginFloat(literal))
+            std::cout << "An error occurred, please try again using an int, char, float or double literal\n";
+    }
+    else if (result == 'd')
+    {
+        if (!convertOriginDouble(literal))
             std::cout << "An error occurred, please try again using an int, char, float or double literal\n";
     }
 }
 
 /*
+u - unknow (não iniciado)
 e - error
 p - pseudo
 c - char
-n - numeric
+i - int
+f - float
+d - double
 */
