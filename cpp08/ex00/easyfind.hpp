cd75c97091c09c634b86622bbb8a59dab9c65cf9
s4ntz <emersonss13@gmail.com>
2023-09-28 13:43:05 -0300

    init cpp08 ex00

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 19:21:25 by esilva-s          #+#    #+#             */
+/*   Updated: 2023/09/28 13:40:02 by esilva-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+T easyfind(T data, int x)
+{
+    
+}
+
+std::ostream&   operator<<( std::ostream &out, const easyfind &ref)
+
+#endif /*EASYFIND_HPP*/
