61eae996ef9f51bbd7b831901c0e7f99d95c9085
s4ntz <emersonss13@gmail.com>
2023-09-28 12:09:09 -0300

    finish cpp07 ex02

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -2,53 +2,79 @@
 #include <cstdlib>
 #include "Array.hpp"
 
-#define MAX_VAL 750
+#define MAX_VAL 20
+
 int main(int, char**)
 {
+    // Declaração com construtor recebendo o tamanho
     Array<int> numbers(MAX_VAL);
+    // vetor padrão
     int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
+
+    // preenchendo com valores aleatorios
+    std::srand(time(NULL));
     for (int i = 0; i < MAX_VAL; i++)
     {
-        const int value = rand();
+        const int value = std::rand();
         numbers[i] = value;
         mirror[i] = value;
     }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
 
+    // Teste sobrecarga de copia
+    Array<int> tmp = numbers;
+
+    // Teste construtor de copia
+    Array<int> test(tmp);
+
+    // Teste comparando o Array com o vetor
+    std::cout << std::endl << "===== Teste comparando o Array com o vetor" << std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
         if (mirror[i] != numbers[i])
         {
-            std::cout << "didn't save the same value!!" << std::endl;
-            return 1;
+            std::cout << "KO | Os valores não são iguais" << std::endl;
+            return (1);
+        }
+        else
+        {
+            std::cout << "OK | index: " << i;
+            std::cout << " mirror: " << mirror[i];
+            std::cout << " numbers: " << numbers[i] << std::endl;
         }
     }
+
+    // Teste index negativo
+    std::cout << std::endl << "===== Teste index negativo" << std::endl;
     try
     {
         numbers[-2] = 0;
+        std::cout << " ERROR!!! INDEX NEGATIVO ACEITO "<< std::endl;
     }
     catch(const std::exception& e)
     {
-        std::cout << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cout << e.what() << '\n';
+        std::cout << e.what() << " - index negativo interceptado\n";
     }
 
+    // Teste index fora do range
+    std::cout << std::endl << "===== Teste index fora do range" << std::endl;
+    try
+    {
+        numbers[MAX_VAL + 1] = 0;
+        std::cout << " ERROR!!! INDEX FORA DO RANGE ACEITO "<< std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << " - index fora do range interceptado\n";
+    }
+
+    // Reescrevendo o Array
+    std::cout << std::endl << "===== Reescrevendo o Array" << std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
-        numbers[i] = rand();
+        numbers[i] = std::rand();
+        std::cout << "index [" << i << "]: " << numbers[i] << std::endl;
     }
-    delete [] mirror;//
+
+    delete [] mirror;
     return 0;
 }
