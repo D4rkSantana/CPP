4cce0ccabf080a9bc159af4430d93a68ee1c357d
esilva-s <esilva-s@student.42sp.org.br>
2023-09-28 23:18:58 +0200

    complete cpp08 ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 19:21:25 by esilva-s          #+#    #+#             */
-/*   Updated: 2023/09/28 13:40:02 by esilva-s         ###   ########.fr       */
+/*   Updated: 2023/09/28 23:18:20 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,29 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
-template <typename T>
-T easyfind(T data, int x)
+class NotFoundException : public std::exception
 {
-    
-}
+	virtual const char* what() const throw(){
+		return ("Not Found !");
+	};
+};
 
-std::ostream&   operator<<( std::ostream &out, const easyfind &ref)
+template <typename Conteiner>
+void easyfind(Conteiner &data, int element)
+{
+    if (std::find(data.begin(), data.end(), element) != data.end())
+	{
+		std::cout << "Found !" << std::endl;
+		return ;
+	}
+	else
+	{
+		throw ::NotFoundException();
+	}
+		
+}
 
 #endif /*EASYFIND_HPP*/
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,14 +6,32 @@
 /*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 21:14:28 by esilva-s          #+#    #+#             */
-/*   Updated: 2023/06/13 21:15:36 by esilva-s         ###   ########.fr       */
+/*   Updated: 2023/09/28 23:18:03 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Template.hpp"
+#include "easyfind.hpp"
 
 int main()
 {
-    std::cout << "Hi world" << std::endl;
+	size_t index = 0;
+
+	// criando e preenchendo os valores do conteiner
+	std::vector<int> inter;
+	
+	while (index < 11)
+	{
+		inter.push_back(static_cast<int>(index));
+		index++;
+	}
+
+	// Procurando o elemento no conteiner
+	try {
+		easyfind(inter, 10);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
     return(0);
 }
