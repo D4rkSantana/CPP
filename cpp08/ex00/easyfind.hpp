b48e911a0d249c2f140fcb1197bd328d3dcfaa27
esilva-s <esilva-s@student.42sp.org.br>
2023-09-28 23:25:46 +0200

    melhoria na exception

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 19:21:25 by esilva-s          #+#    #+#             */
-/*   Updated: 2023/09/28 23:18:20 by esilva-s         ###   ########.fr       */
+/*   Updated: 2023/09/28 23:25:18 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@
 
 class NotFoundException : public std::exception
 {
-	virtual const char* what() const throw(){
-		return ("Not Found !");
-	};
+	virtual const char* what() const throw(){ return ("Not Found !");}
 };
 
 template <typename Conteiner>
